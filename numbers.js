2028b482bdb056e93913b353d8fb368b7ae2c3cd
Your Name <rzhyl05@gmail.com>
2024-09-13 16:54:05 +0800

    your commit message

---
+++ numbers.js
@@ -0,0 +1,20 @@
+let firstFavNum = 10;
+let secondFavNum = 4;
+
+let additionResult = firstFavNum + secondFavNum;
+console.log('Addition:', additionResult);
+
+let subtractionResult = firstFavNum - secondFavNum;
+console.log('Subtraction:', subtractionResult);
+
+let multiplicationResult = firstFavNum * secondFavNum;
+console.log('Multiplication:', multiplicationResult);
+
+let divisionResult = firstFavNum / secondFavNum;
+console.log('Division:', divisionResult);
+
+let modulusResult = firstFavNum % secondFavNum;
+console.log('Modulus:', modulusResult);
+
+let exponentiationResult = firstFavNum ** secondFavNum;
+console.log('Exponentiation (Power):', exponentiationResult);
